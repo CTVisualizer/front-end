a759f93eb6f3d7f34a1ccc73461713bdac461ea0
Connor Boyle <coboyle@proofpoint.com>
2018-07-21 13:46:06 -0400

    Finished getRun.sh

--- getRun.sh
+++ getRun.sh
@@ -1,4 +1,8 @@
-NAME="CTV.tar.gz"
+OUTDIR="CTV"
+NAME="$OUTDIR.tar.gz"
+rm -rf $OUTDIR
 rm $NAME || true
 wget -L --header "PRIVATE-TOKEN: TOKEN" "https://gitlab.com/ctvisualizer/front-end/-/jobs/artifacts/build-pipeline/raw/CTVisualizer-darwin-x64.tar.gz?job=buildTar" -O "./$NAME"
-tar -xzvf $NAME
+mkdir $OUTDIR
+tar -xzf $NAME -C $OUTDIR
+open "$OUTDIR/CTVisualizer.app"
