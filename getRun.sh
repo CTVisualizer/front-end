d194d82ecba433ea7a2f0d77306f4ab4b8d51d34
Connor Boyle <coboyle@proofpoint.com>
2018-07-21 14:03:57 -0400

    lldb

--- getRun.sh
+++ getRun.sh
@@ -5,4 +5,4 @@ rm $NAME || true
 wget -L --header "PRIVATE-TOKEN: TOKEN" "https://gitlab.com/ctvisualizer/front-end/-/jobs/artifacts/build-pipeline/raw/CTVisualizer-darwin-x64.tar.gz?job=buildTar" -O "./$NAME"
 mkdir $OUTDIR
 tar -xzf $NAME -C $OUTDIR
-open "$OUTDIR/CTVisualizer.app"
+lldb "$OUTDIR/CTVisualizer.app"
