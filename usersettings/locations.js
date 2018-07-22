8d2879e38798fb91a90dce56c15ee97f3dd763a2
Connor Boyle <coboyle@proofpoint.com>
2018-07-21 22:12:44 -0400

    Fixing launch bug

--- usersettings/locations.js
+++ usersettings/locations.js
@@ -1,2 +1,2 @@
 const fs = require("fs");
-module.exports = (function() { return JSON.parse(fs.readFileSync("./usersettings/locations.json"))})()
+module.exports = (function() { return require("./locations.json")})()
