f2eded3fea569a06650831e7e89384ab435d8228
Connor Boyle <coboyle@proofpoint.com>
2018-07-21 21:37:05 -0400

    getRun is now generic

--- getRun.sh
+++ getRun.sh
@@ -2,7 +2,7 @@ OUTDIR="CTV"
 NAME="$OUTDIR.tar.gz"
 rm -rf $OUTDIR
 rm $NAME || true
-wget -L --header "PRIVATE-TOKEN: TOKEN" "https://gitlab.com/ctvisualizer/front-end/-/jobs/artifacts/build-pipeline/raw/CTVisualizer-darwin-x64.tar.gz?job=buildTar" -O "./$NAME"
+wget -L --header "PRIVATE-TOKEN: <GITLAB-TOKEN>" "https://gitlab.com/ctvisualizer/front-end/-/jobs/artifacts/<branch>/raw/CTVisualizer-darwin-x64.tar.gz?job=buildTar" -O "./$NAME"
 mkdir $OUTDIR
 tar -xzf $NAME -C $OUTDIR
 lldb "$OUTDIR/CTVisualizer.app"
